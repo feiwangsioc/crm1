b84d0f5d7de590ad817265c7bbf84bc0805573a4
Fei Wang <feiwangsioc@gmail.com>
2020-05-30 15:14:21 -0400

    profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
-
+	profile_pic = models.ImageField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
@@ -35,9 +37,6 @@ class Product(models.Model):
 	def __str__(self):
 		return self.name
 
-
-
-
 class Order(models.Model):
 	STATUS = (
 			('Pending', 'Pending'),
@@ -49,5 +48,9 @@ class Order(models.Model):
 	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	note =  models.CharField(max_length=1000, null=True)
 
-	
+	def __str__(self):
+		return self.product.name
+
+
